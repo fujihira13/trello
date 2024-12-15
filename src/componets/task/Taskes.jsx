47d66ce1acd836765562fb4d7ec6fd567f027c2d
masashi.fujihira <masa.1113.1991@gmail.com>
2024-12-15 17:17:09 +0900

    ドラックアンドドロップのバグ

--- src/componets/task/Taskes.jsx
+++ src/componets/task/Taskes.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
-import Task from './Task'
-import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd'
+import React from "react";
+import Task from "./Task";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 
-const Taskes = ({taskList,setTaskList}) => {
+const Taskes = ({ taskList, setTaskList }) => {
   const handleDragEnd = (result) => {
-    //タスク並び替え
-    const remove = taskList.splice(result.source.index,1)
-    taskList.splice(result.destination.index,0,remove[0])
-    setTaskList(taskList);
+    // ドロップ先が無い場合は何もしない
+    if (!result.destination) {
+      return;
+    }
+
+    // 配列の複製を作成
+    const newTaskList = Array.from(taskList);
+
+    // 移動元のアイテムを削除
+    const [removed] = newTaskList.splice(result.source.index, 1);
+
+    // 移動先に挿入
+    newTaskList.splice(result.destination.index, 0, removed);
+
+    // 状態を更新
+    setTaskList(newTaskList);
   };
   return (
     <div>
       <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId='droppable'>
+        <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              {taskList.map((task,index) => (
-              <div key={task.id}>
-                 <Task index = {index} task = {task} taskList = {taskList} setTaskList = {setTaskList} />
-              </div>
+              {taskList.map((task, index) => (
+                <div key={task.id}>
+                  <Task
+                    index={index}
+                    task={task}
+                    taskList={taskList}
+                    setTaskList={setTaskList}
+                  />
+                </div>
               ))}
               {provided.placeholder}
-            </div>)}      
-         </Droppable>
+            </div>
+          )}
+        </Droppable>
       </DragDropContext>
     </div>
-  )
-}
+  );
+};
 
-export default Taskes
+export default Taskes;
